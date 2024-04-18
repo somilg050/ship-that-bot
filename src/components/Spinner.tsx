790288ec6d7fc473e6dd505a7df4bf828ce72a41
somilg050 <somilg050@gmail.com>
2024-04-18 14:46:52 +0530

    minor tweak in spinner color

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,18 +1,18 @@
 const Spinner = () => {
-    return (
-        <div className="animate-pulse flex space-x-4">
-            <div className="flex-1 space-y-1 py-1">
-                <div className="h-2 bg-slate-700 rounded"></div>
-                <div className="space-y-1">
-                    <div className="grid grid-cols-3 gap-4">
-                        <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                        <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                    </div>
-                    <div className="h-2 bg-slate-700 rounded"></div>
-                </div>
-            </div>
+  return (
+    <div className="flex animate-pulse space-x-4">
+      <div className="flex-1 space-y-1 py-1">
+        <div className="h-2 rounded bg-slate-400"></div>
+        <div className="space-y-1">
+          <div className="grid grid-cols-3 gap-4">
+            <div className="col-span-2 h-2 rounded bg-slate-500"></div>
+            <div className="col-span-1 h-2 rounded bg-slate-500"></div>
+          </div>
+          <div className="h-2 rounded bg-slate-600"></div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Spinner;
